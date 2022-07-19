e976fbe83d28c61a74b44813644ff4dc67c380ba
Eve Morgan <evemorgan27@gmail.com>
2022-07-19 12:09:35 -0700

    initla files for getting cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
 
+
+const BASE_URL = "http://deckofcardsapi.com/api/deck/";
+
 function App() {
+  const [deck, setDeck] = useState(null);
+  const [card, setCard] = useState(null);
+
+
+  async function fetchDeck() {
+    const deckResult = await axios.get(`${BASE_URL}new/`);
+    setDeck(deckResult.data);
+  }
+
+  useEffect(function fetchCardWhenMounted() {
+    fetchDeck();
+  }, []);
+
+  //console.log(deck);
+
+  async function fetchCardUsingIdOnButtonClick() {
+    const cardResult = await axios.get(`${BASE_URL}/${deck.deck_id}/draw/?count=1`);
+    const cardInfo = cardResult.data.cards[0];
+    setCard( cardInfo.value, cardInfo.suit);
+  }
+
+  console.log()
+
+
+  // get card using deck id by axios get request
+  // changes cardname array
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Deck of cards</p>
+        <button>Get card</button>
       </header>
     </div>
   );
